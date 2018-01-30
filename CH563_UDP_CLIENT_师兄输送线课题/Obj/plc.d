68c0fa3b32be6adc7b629c7e2db2255aaf971a95
danyanshuyu <964057364@qq.com>
2018-01-30 11:25:29 +0800

    Revert "Revert "工程导入""
    This reverts commit 9ec39141510b6e7aff9a7c0dca9926d6640b82d7.

---
+++ CH563_UDP_CLIENT_师兄输送线课题/Obj/plc.d
@@ -0,0 +1,4 @@
+..\Obj\plc.o: ..\User\PLC.c
+..\Obj\plc.o: ..\User\Extern_Variables.h
+..\Obj\plc.o: ..\SRC\CH563SFR.H
+..\Obj\plc.o: ..\SRC\CH563BAS.H
---
+++ CH563_UDP_CLIENT_师兄输送线课题/Obj/sysfreq.d
@@ -0,0 +1,5 @@
+..\Obj\sysfreq.o: ..\SRC\SYSFREQ.C
+..\Obj\sysfreq.o: ..\SRC\CH563SFR.H
+..\Obj\sysfreq.o: ..\SRC\CH563BAS.H
+..\Obj\sysfreq.o: ..\SRC\SYSFREQ.H
+..\Obj\sysfreq.o: ..\SRC\CH563SFR.H
